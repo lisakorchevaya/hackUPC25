44901053f6c9e9c8b8110709f160e1b6a9c07951
antimonicre <monicjimenez@hotmail.com>
2025-05-03 20:48:53 +0200

    borrado backend

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify, request
-from models import db, User  # Importamos el DB y el modelo User
-
-app = Flask(__name__)
-
-# Configurando la base de datos SQLite
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
--- models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, Float, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-class Registro(Base):
-    __tablename__ = 'registros'
-
-## ESTRUCTURA DE BASE DE DATOS ##
